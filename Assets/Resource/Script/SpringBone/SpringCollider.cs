bd602e0bec5a13819240d2f60c798e87591a3b62
yunon <38744862+yunonXD@users.noreply.github.com>
2022-05-30 01:11:04 +0900

    IKUpdate V5 오프셋 조정

---
+++ Assets/Resource/Script/SpringBone/SpringCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityChan
+{
+	public class SpringCollider : MonoBehaviour
+	{
+		public float radius = 0.5f;
+
+		private void OnDrawGizmosSelected ()
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere (transform.position, radius);
+		}
+	}
+}
